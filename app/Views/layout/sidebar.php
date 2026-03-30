9989d89050f5affb0166c552f3cd6f5b7595743a
CIZZLGrid <pxeco91103@gmail.com>
2026-03-30 13:16:27 +0800

    added submen sim cards + js script

--- app/Views/layout/sidebar.php
+++ app/Views/layout/sidebar.php
@@ -1,19 +1,36 @@
-<div class="sidebar">
-    <ul class="menu">
-        <li><i class="fa fa-home"></i> Dashboard</li>
+<body>
+    <div class="sidebar">
+        <ul class="menu">
+            <li><i class="fa fa-home"></i> Dashboard</li>
 
-        <li class="submenu">
-            <i class="fa fa-user"></i> User Management
-        </li>
+            <li class="submenu">
+                <i class="fa fa-user"></i> User Management
+            </li>
 
-        <li><i class="fa fa-list"></i> Categories</li>
+            <li><i class="fa fa-list"></i> Categories</li>
 
-        <li class="menu-title">Products</li>
-        <li class="active">Manage Products</li>
-        <li>Add Products</li>
+            <li class="menu-title">Sim Card Management</li>
 
-        <li><i class="fa fa-image"></i> Media Files</li>
-        <li><i class="fa fa-shopping-cart"></i> Sales</li>
-        <li><i class="fa fa-chart-bar"></i> Sales Report</li>
-    </ul>
-</div>
+            <li><i class="fa fa-sim-card"></i> Manage Sim Card</li>
+
+            <!-- Parent menu with submenu -->
+            <li class="has-submenu active">
+               Add Sim Card
+                <ul class="submenu">
+                    <li>Add Globe Sim Cards</li>
+                    <li>Add Smart Sim Cards</li>
+                </ul>
+            </li>
+
+            <li><i class="fa fa-image"></i> Media Files</li>
+        </ul>
+    </div>
+    <script>
+    const submenuParents = document.querySelectorAll('.menu li.has-submenu');
+    submenuParents.forEach(parent => {
+        parent.addEventListener('click', () => {
+            parent.classList.toggle('active');
+        });
+    });
+</script>
+</body>
